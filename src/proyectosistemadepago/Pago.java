f459cd5513acc560b5fccf03d0b734037facc08a
T <T@PC267>
2014-09-29 19:47:44 -0500

    mi primera subida

---
+++ src/proyectosistemadepago/Pago.java
@@ -0,0 +1,17 @@
+
+
+package proyectosistemadepago;
+
+
+public class Pago {
+    
+    public Pago(){
+        System.out.println("aver si aparesco");
+    }
+    
+public float hacerpago(int horasTrabajadas, int sueldo){
+    float pago=horasTrabajadas*sueldo;
+    return pago;
+    
+     }
+}
